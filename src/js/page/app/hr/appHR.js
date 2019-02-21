8a19deeac6f8ae72ef21a54fb5fc3f421d7d0d67
liyancode <liyan.code@gmail.com>
2019-02-22 00:12:13 +0800

    icon button

--- src/js/page/app/hr/appHR.js
+++ src/js/page/app/hr/appHR.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Layout, Breadcrumb, Tabs, Collapse, Spin, Icon, Table, Button} from 'antd';
+import {Layout, Breadcrumb, Tabs, Collapse, Spin, Icon,Row,Col, Divider,Table, Button} from 'antd';
 import _globalConstrants from "../../../util/_globalConstrants"
 import _globalUtil from "../../../util/_globalUtil"
 import {serviceUser} from "../../../service/serviceUser";
@@ -154,39 +154,121 @@ const PageContent = (props) => {
     }
 
 
-    return (
-        <Tabs defaultActiveKey={props.sub} onChange={props.tabChange} onTabClick={props.tabClick}>
-            <TabPane tab="个人中心" key={subConstrants.hrMy}>
-                <Spin spinning={props.loading} tip="加载中..." size="large">
-                    <Collapse defaultActiveKey={['1']}>
-                        <Panel header="工作信息" key="1">
-                            {workInfo}
-                        </Panel>
-                        <Panel header="个人信息" key="2">
-                            {personalInfo}
-                        </Panel>
-                    </Collapse>
-                </Spin>
-            </TabPane>
-            <TabPane tab="申请单管理" key={subConstrants.hrApplication}>
-                <div>
-                    <Button style={btnStyle} type="primary" icon="plus" myattr={"leave"}
-                            onClick={props.createButtonClick}>请假单<Icon type="schedule"/></Button>
-                    <Button style={btnStyle} icon="plus" myattr={"office_supplies"} onClick={props.createButtonClick}>办公用品申请<Icon
-                        type="laptop"/></Button>
-                    <Button style={{
-                        marginRight: 8,
-                        marginBottom: 12,
-                        float: "right"
-                    }} type="danger" icon="plus" myattr={"resignation"}
-                            onClick={props.createButtonClick}>离职申请<Icon
-                        type="frown"/></Button>
-                </div>
-                <Table rowKey="application_id" columns={applicationsTableColumns}
-                       dataSource={props.userApplications} size="small"/>
-            </TabPane>
-            <TabPane tab="部门信息" key={subConstrants.hrDepartment}>部门信息(开发中...)</TabPane>
-        </Tabs>)
+    // return (
+    //     <Tabs defaultActiveKey={props.sub} onChange={props.tabChange} onTabClick={props.tabClick}>
+    //         <TabPane tab="个人中心" key={subConstrants.hrMy}>
+    //             <Spin spinning={props.loading} tip="加载中..." size="large">
+    //                 <Collapse defaultActiveKey={['1']}>
+    //                     <Panel header="工作信息" key="1">
+    //                         {workInfo}
+    //                     </Panel>
+    //                     <Panel header="个人信息" key="2">
+    //                         {personalInfo}
+    //                     </Panel>
+    //                 </Collapse>
+    //             </Spin>
+    //         </TabPane>
+    //         <TabPane tab="申请单管理" key={subConstrants.hrApplication}>
+    //             <div>
+    //                 <Button style={btnStyle} type="primary" icon="plus" myattr={"leave"}
+    //                         onClick={props.createButtonClick}>请假单<Icon type="schedule"/></Button>
+    //                 <Button style={btnStyle} icon="plus" myattr={"office_supplies"} onClick={props.createButtonClick}>办公用品申请<Icon
+    //                     type="laptop"/></Button>
+    //                 <Button style={{
+    //                     marginRight: 8,
+    //                     marginBottom: 12,
+    //                     float: "right"
+    //                 }} type="danger" icon="plus" myattr={"resignation"}
+    //                         onClick={props.createButtonClick}>离职申请<Icon
+    //                     type="frown"/></Button>
+    //             </div>
+    //             <Table rowKey="application_id" columns={applicationsTableColumns}
+    //                    dataSource={props.userApplications} size="small"/>
+    //         </TabPane>
+    //         <TabPane tab="部门信息" key={subConstrants.hrDepartment}>部门信息(开发中...)</TabPane>
+    //     </Tabs>
+    // )
+    const styleFLogo={
+        display:"inline-block",
+        minWidth:88,
+        height:88,
+        textAlign:"center",
+        verticalAlign:"middle",
+        backgroundColor:"#fff",
+        border:"1px solid #d9d9d9",
+        borderRadius:"4px",
+        outline:0,
+        cursor:"pointer",
+        padding:8,
+        margin:8
+    }
+    // display: inline-block;
+    // min-width: 32px;
+    // height: 32px;
+    // text-align: center;
+    // vertical-align: middle;
+    return (<div>
+        <Divider orientation="left">办公人事管理</Divider>
+        <Row gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
+            <Col className="gutter-row">
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"user"}/>
+                    <span style={{display:"block",marginLeft:0}}>我的信息</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"desktop"}/>
+                    <span style={{display:"block",marginLeft:0}}>办公用品</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"solution"}/>
+                    <span style={{display:"block",marginLeft:0}}>员工档案</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"schedule"}/>
+                    <span style={{display:"block",marginLeft:0}}>请假调休</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"property-safety"}/>
+                    <span style={{display:"block",marginLeft:0}}>报销管理</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"user-delete"}/>
+                    <span style={{display:"block",marginLeft:0}}>离职申请</span>
+                </Button>
+            </Col>
+        </Row>
+        <Divider orientation="left">信息通讯管理</Divider>
+        <Row gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
+            <Col className="gutter-row">
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"notification"}/>
+                    <span style={{display:"block",marginLeft:0}}>通知公告</span>
+                </Button>
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"read"}/>
+                    <span style={{display:"block",marginLeft:0}}>规章制度</span>
+                </Button>
+            </Col>
+        </Row>
+        <Divider orientation="left">资料档案管理</Divider>
+        <Row gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
+            <Col className="gutter-row">
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"usb"}/>
+                    <span style={{display:"block",marginLeft:0}}>电子资料</span>
+                </Button>
+            </Col>
+        </Row>
+        <Divider orientation="left">员工考核管理</Divider>
+        <Row gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
+            <Col className="gutter-row">
+                <Button style={styleFLogo} onClick={props.testClick}>
+                    <Icon style={{fontSize:36}} type={"trophy"}/>
+                    <span style={{display:"block",marginLeft:0}}>绩效考核</span>
+                </Button>
+            </Col>
+        </Row>
+    </div>)
 }
 
 export default class AppHR extends React.Component {
@@ -219,6 +301,7 @@ export default class AppHR extends React.Component {
         this.handleTabClick = this.handleTabClick.bind(this);
         this.handleTabChange = this.handleTabChange.bind(this);
         this.updateSubData = this.updateSubData.bind(this);
+        this.testClick = this.testClick.bind(this);
     }
 
     updateSubData(sub) {
@@ -254,6 +337,9 @@ export default class AppHR extends React.Component {
         this.updateSubData(e)
     }
 
+    testClick(e){
+        console.log(e.target)
+    }
     render() {
         return (
             <div>
@@ -268,6 +354,7 @@ export default class AppHR extends React.Component {
                                  sub={this.state.sub}
                                  userInfo={this.state.userInfo}
                                  userApplications={this.state.userApplications}
+                                 testClick={this.testClick}
                     />
                 </Content>
             </div>)
