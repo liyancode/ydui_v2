7c507f891f5715d096f4b8175f1c93e034f6565a
liyancode <liyan.code@gmail.com>
2019-02-26 23:27:01 +0800

    login path correct

--- src/js/service/tokenExpired.js
+++ src/js/service/tokenExpired.js
@@ -6,7 +6,7 @@ export function tokenExpired() {
         content: '你需要重新登录，现在操作吗？',
         onOk() {
             localStorage.removeItem('user');
-            window.location.replace('/#/login')
+            window.location.replace('/login')
         },
         onCancel() {
             message.error("登录信息过期，请重新登录！");
